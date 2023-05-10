cf3d9f913fd24d7ca27d64d3f08ab05cbbcdf3da
Shahar Asher <shaharas30@gmail.com>
2023-05-10 21:24:25 +0300

    add more problem and change file name of the first one

---
+++ euler-1-Multiples_of_3_or_5.py
@@ -0,0 +1,17 @@
+""" Id = 1 """
+
+# First idea
+MAX = 999
+
+def divFunc(div_n):
+    p = MAX // div_n
+    return div_n * (p * (p+1))//2
+    
+# Second idea
+print(divFunc(3) + divFunc(5) - divFunc(3*5))
+
+lst = list(range(1, 1000))
+sum_final_lst = sum([x for x in lst if x % 3 == 0 or x % 5 == 0])
+print(sum_final_lst)
+
+
