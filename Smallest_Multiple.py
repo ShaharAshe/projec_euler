4c61a3aca3e3828740917ef8fb354ec373c001a0
ShaharAshe <shaharas30@gmail.com>
2024-03-19 14:54:45 +0200

    smallestMultiple

---
+++ Smallest_Multiple.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+# === first idea ===
+def is_prime(num: int)->bool:
+    if num == 0 or num == 1:
+        return False
+    for n in range(2, int(np.sqrt(num))+1):
+        if num % n == 0:
+            return False
+    return True
+
+def smallestMultiple(maxRange: int)->int:
+    if maxRange < 1:
+        return 0
+    
+    smallest_M = 1
+    for i in range(1, maxRange+1):
+        if is_prime(i):
+            power = 1
+            while(np.power(i, power) <= maxRange):
+                smallest_M *= i
+                power += 1
+    return smallest_M
+
+
+if __name__ == '__main__':
+    print(smallestMultiple(20))
+        
