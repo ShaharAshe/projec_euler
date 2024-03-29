b87afd279f445f23a5d17e24f38160a553f04ff2
ShaharAshe <shaharas30@gmail.com>
2024-03-29 10:47:27 +0300

    10001st Primes

---
+++ euler-7-10001st_Prime.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def is_prime(primes, i)->bool:
+    for p in primes:
+        if p > np.sqrt(i):
+            return True
+        if i%p == 0:
+            return False
+    return True
+                
+
+def nPrime(num:int)->int:
+    primes = []
+    i = 2
+    while(True):
+        if i == 2:
+            primes = [2]
+        elif is_prime(primes, i):
+             primes += [i]
+        if len(primes) == num:
+            return i
+        i += 1
+
+
+if __name__ == "__main__":
+    print(nPrime(10001))
